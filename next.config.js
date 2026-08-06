deb1b8408df4e61eee8945c081842a2ac15a5beb
Prateek Mittal <prateek.mittal@grapecity.com>
2026-08-06 23:49:37 +0530

    Update Pages workflow and Next.js export config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
